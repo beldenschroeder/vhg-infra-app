16a550c4191245428e347ad3c0c0f47a16057815
Belden Schroeder <beldenschroeder@gmail.com>
2024-07-04 11:41:27 -0700

    Set `null` default values

--- variables.tf
+++ variables.tf
@@ -1,42 +1,51 @@
 variable "region" {
-  default = "us-east-1"
   description = "AWS region"
+  default = "us-east-1"
 }
 
 variable "remote_state_bucket" {
   description = "S3 bucket to store Terraform state file"
+  default = null
 }
 
 variable "remote_state_key" {
   description = "S3 key to store Terraform state file"
+  default = null
 }
 
 # Application Variables for Task Definition
 
 variable "ecs_service_name" {
   description = "ECS service name"
+  default = null
 }
 
 variable "docker_image_url" {
   description = "Docker image URL"
+  default = null
 }
 
 variable "memory" {
   description = "Memory for ECS task"
+  default = null
 }
 
 variable "docker_container_port" {
   description = "Docker container port"
+  default = null
 }
 
 variable "desired_task_number" {
   description = "Desired number of tasks"
+  default = null
 }
 
 variable "vercel_env" {
   description = "Vercel environment"
+  default = null
 }
 
 variable "vercel_url" {
   description = "Vercel URL"
+  default = null
 }

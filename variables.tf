ca7d528d51ec29a67a47c8f250f17b1c2ddb10a3
Belden Schroeder <beldenschroeder@gmail.com>
2024-07-09 16:01:28 -0700

    Add types to variables

--- variables.tf
+++ variables.tf
@@ -1,51 +1,51 @@
 variable "region" {
   description = "AWS region"
-  default = "us-east-1"
+  type = string
 }
 
 variable "remote_state_bucket" {
   description = "S3 bucket to store Terraform state file"
-  default = null
+  type = string
 }
 
 variable "remote_state_key" {
   description = "S3 key to store Terraform state file"
-  default = null
+  type = string
 }
 
 # Application Variables for Task Definition
 
 variable "ecs_service_name" {
   description = "ECS service name"
-  default = null
+  type = string
 }
 
 variable "docker_image_url" {
   description = "Docker image URL"
-  default = null
+  type = string
 }
 
 variable "memory" {
   description = "Memory for ECS task"
-  default = null
+  type = number
 }
 
 variable "docker_container_port" {
   description = "Docker container port"
-  default = null
+  type = number
 }
 
 variable "desired_task_number" {
   description = "Desired number of tasks"
-  default = null
+  type = number
 }
 
 variable "vercel_env" {
   description = "Vercel environment"
-  default = null
+  type = string
 }
 
 variable "vercel_url" {
   description = "Vercel URL"
-  default = null
+  type = string
 }
